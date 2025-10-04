84e4c59904b681405a95bcb11717212af706dc5b
Jean-Chérif Ayanou <jean-cherif.ayanou@nurova.ca>
2025-10-03 20:24:37 -0400

    wip

--- gna-page-maintenance (1)/constants.tsx
+++ gna-page-maintenance (1)/constants.tsx
@@ -14,6 +14,7 @@ const ConsultingIcon = () => (
     </svg>
 );
 
+
 const CybersecurityIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 20.944a12.02 12.02 0 009 3c4.532 0 8.397-2.93 9.618-7.056a11.955 11.955 0 00-2.318-7.984z" />
