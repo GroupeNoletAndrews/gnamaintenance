b6b4e8bcb228686f5e3e5c5a3bbf7c06a151110f
Jean-Chérif Ayanou <jean-cherif.ayanou@nurova.ca>
2025-10-03 20:16:43 -0400

    wip

--- gna-page-maintenance (1)/vite.config.ts
+++ gna-page-maintenance (1)/vite.config.ts
@@ -18,6 +18,17 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        rollupOptions: {
+          output: {
+            manualChunks: undefined,
+          },
+        },
+      },
+      base: './'
     };
 });
